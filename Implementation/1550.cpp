d14fca8f9720bc30b321dbfe933ae581e09bd54e
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-03-28 13:41:38 +0900

    hex

---
+++ Implementation/1550.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    int num;
+    cin >> hex >> num;
+    cout << num;
+    return 0;
+}
