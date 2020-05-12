814e89da3a2c32ae91c770d708d01633510b85a8
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-05-12 21:38:23 +0900

    Going up to stairs

--- DP/2579.cpp
+++ DP/2579.cpp
@@ -16,8 +16,7 @@ int main()
     int dp[301] = { 0, };
     dp[1] = arr[1];
     dp[2] = arr[1] + arr[2];
-    dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]);
-    for (int i = 4; i <= N; ++i)
+    for (int i = 3; i <= N; ++i)
         dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
     cout << dp[N];
     return 0;
