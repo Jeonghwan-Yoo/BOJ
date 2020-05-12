07433f760cfb7ddaeed20391bde3208f80886c78
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-05-12 21:39:20 +0900

    Grape wine tasting

--- DP/2156.cpp
+++ DP/2156.cpp
@@ -16,8 +16,7 @@ int main()
     int dp[10'001] = { 0, };
     dp[1] = arr[1];
     dp[2] = arr[1] + arr[2];
-    dp[3] = max({ dp[2], arr[1] + arr[3], arr[2] + arr[3] });
-    for (int i = 4; i <= N; ++i)
+    for (int i = 3; i <= N; ++i)
     {
         dp[i] = max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
         dp[i] = max(dp[i], dp[i - 1]);
