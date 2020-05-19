439e37ba30a952b5201be15175c9028795bde9cb
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-05-19 12:30:33 +0900

    Fibonacci number 5

---
+++ DP/10870.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    int dp[21] = { 0, };
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; ++i)
+        dp[i] = dp[i - 1] + dp[i - 2];
+    cout << dp[n];
+    
+    return 0;
+}
