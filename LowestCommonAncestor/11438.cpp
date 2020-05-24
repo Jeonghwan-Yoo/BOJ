78b2355dd3261d52066956ec5a314080af928785
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-05-24 22:03:45 +0900

    LCA 2

--- LowestCommonAncestor/11438.cpp
+++ LowestCommonAncestor/11438.cpp
@@ -10,51 +10,61 @@ using namespace std;
 
 int N;
 vector<int> adj[MAX];
-int parent[LOG2MAX][MAX];
-bool visited[MAX];
+vector<int> idx[(MAX << 1) - 1];
 int depth[MAX];
+int euler[(MAX << 1) - 1];
+int pos;
 
-void Dfs(int src, int d)
+void Dfs(int src, int parent, int d)
 {
-    visited[src] = true;
     depth[src] = d;
+    ++pos;
+    euler[pos] = src;
+    idx[src].push_back(pos);
     for (auto &a : adj[src])
     {
-        if (visited[a] == true)
-            continue;
-        parent[0][a] = src;
-        Dfs(a, d + 1);
+        if (a != parent)
+        {
+            Dfs(a, src, d + 1);
+            ++pos;
+            euler[pos] = src;
+            idx[src].push_back(pos);
+        }
     }
 }
 
+int pow2[LOG2MAX];
+int lg2[(MAX << 1) - 1];
+pair<int, int> depthNode[LOG2MAX][(MAX << 1) - 1];
+
 void SparseTable()
 {
+    pow2[0] = 1;
     for (int i = 1; i < LOG2MAX; ++i)
-        for (int j = 1; j <= N; ++j)
-            parent[i][j] = parent[i - 1][parent[i - 1][j]];
+        pow2[i] = pow2[i - 1] << 1;
+
+    for (int i = 0; i < LOG2MAX; ++i)
+        lg2[pow2[i]] = i;
+    for (int i = 1; i < (MAX << 1) - 1; ++i)
+        if (lg2[i] == 0)
+            lg2[i] = lg2[i - 1];
+
+    for (int i = 1; i <= (N << 1) - 1; ++i)
+        depthNode[0][i] = { depth[euler[i]], euler[i] };
+    for (int i = 1; i < LOG2MAX; ++i)
+        for (int j = 1; j <= (N << 1) - 1; ++j)
+            depthNode[i][j] = min(depthNode[i - 1][j], depthNode[i - 1][j + pow2[i - 1]]);
 }
 
 int Lca(int a, int b)
 {
-    if (depth[a] > depth[b])
-        swap(a, b);
-    
-    for (int i = LOG2MAX - 1; i >= 0; --i)
-        if (depth[b] - depth[a] >= (1 << i))
-            b = parent[i][b];
+    int l = idx[a][0];
+    int r = idx[b][0];
+    if (l > r)
+        swap(l, r);
 
-    if (a == b)
-        return a;
-    
-    for (int i = LOG2MAX - 1; i >= 0; --i)
-    {
-        if (parent[i][a] != parent[i][b])
-        {
-            a = parent[i][a];
-            b = parent[i][b];
-        }
-    }
-    return parent[0][a];
+    int sz = lg2[r - l + 1];
+    return min(depthNode[sz][l], depthNode[sz][r - pow2[sz] + 1]).second;
 }
 
 int main()
@@ -71,7 +81,7 @@ int main()
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    Dfs(1, 0);
+    Dfs(1, 0, 0);
     SparseTable();
 
     int M;
