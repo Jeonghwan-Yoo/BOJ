df50f415e859be048b4c6330707cacbf671ebbe6
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-09-10 23:27:40 +0900

    N and M 7

---
+++ Backtracking/15656.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+
+void Backtracking(vector<int> &sub, vector<int> const &v)
+{
+    if ((int)sub.size() == M)
+    {
+        for (int i = 0; i < M; ++i)
+            cout << sub[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
+        sub.emplace_back(v[i]);
+        Backtracking(sub, v);
+        sub.pop_back();
+    }
+}
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N >> M;
+    vector<int> v(N), sub, visited(N);
+    for (int i = 0; i < N; ++i)
+        cin >> v[i];
+    sort(v.begin(), v.end());
+
+    Backtracking(sub, v);
+
+    return 0;
+}
