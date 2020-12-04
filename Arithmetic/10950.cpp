b6bb133787cc51f13725f264aaaa922fab0fc664
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-12-05 03:29:06 +0900

    A+B - 3

---
+++ Arithmetic/10950.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B;
+        cin >> A >> B;
+        cout << A + B << '\n';
+    }
+    
+    return 0;
+}
