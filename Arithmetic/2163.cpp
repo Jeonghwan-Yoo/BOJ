f20796cfec38a4497910fae62176b867a2172b51
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-12-06 15:52:08 +0900

    Cutting a chocolate

---
+++ Arithmetic/2163.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N, M;
+    cin >> N >> M;
+    cout << N * M - 1;
+
+    return 0;
+}
